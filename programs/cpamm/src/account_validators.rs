05aac5ee895db3114820ac89ac80e57865db939d
Aman <89671495+vineam@users.noreply.github.com>
2021-11-30 16:04:41 -0800

    Add input/output safety checks (#44)
    * fix unused keypair for intermediate swap outputs
    
    * add safety checks for user input/output destinations
    
    * remove redundant user input validation & fmt

--- programs/cpamm/src/account_validators.rs
+++ programs/cpamm/src/account_validators.rs
@@ -83,6 +83,13 @@ impl<'info> Validate<'info> for Swap<'info> {
             } else {
                 (&self.user.swap.token_1, &self.user.swap.token_0)
             };
+
+        assert_keys_eq!(
+            self.output.user.owner,
+            self.user.user_authority,
+            "output.user.owner must be user.user_authority"
+        );
+
         self.input.validate_for_swap(swap_input)?;
         self.output.validate_for_swap(swap_output)?;
 
