087e07dda3eaba73a1945c9b4054ba484491ac3d
Johanna Johansson <anna@matcha.finance>
2022-03-20 17:30:37 +0200

    Export SuperCoder

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
+import { SuperCoder } from "@saberhq/anchor-contrib";
 import { Percent, u64 } from "@saberhq/token-utils";
 import { PublicKey } from "@solana/web3.js";
 
+import type { CpAmm } from "./programs";
+import { CpammJSON } from "./programs";
 import type { Fees } from "./wrappers/cp-amm/types";
 
 export const LOCAL_CHAIN_ID = 100;
@@ -8,7 +11,11 @@ export const LOCAL_CHAIN_ID = 100;
 export const DEFAULT_DECIMALS = 6;
 export const DEFAULT_HARD_CAP = new u64("10000000000000000"); // 10 billion
 
-// maps to base: SENfrUReUHhi9sM4uLqiZvWBSBTYjJDrpK1Grvsg6Tr
+/**
+ * The mainnet (and devnet) Factory address.
+ *
+ * This address maps to the base: SENfrUReUHhi9sM4uLqiZvWBSBTYjJDrpK1Grvsg6Tr
+ */
 export const DEFAULT_FACTORY = new PublicKey(
   "DtXfMx19KqLfUWJGPMdkjxfbzQ4PrHyBQhA7vgQ2oRzG"
 );
@@ -23,3 +30,11 @@ export const DEFAULT_FEES: Fees = {
   adminTrade: new Percent(1_600, 10_000),
   adminWithdraw: new Percent(0, 10_000),
 };
+
+/**
+ * {@link SuperCoder} for the CpAmm program.
+ */
+export const CPAMM_CODER = new SuperCoder<CpAmm>(
+  PROGRAM_ADDRESSES.CpAmm,
+  CpammJSON
+);
